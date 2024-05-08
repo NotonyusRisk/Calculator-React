c496a52e4e6e1602c73263170b441b4b505ce7c5
Antonio <antoniojrudass@gmail.com>
2024-05-08 18:12:17 -0500

    feat::files react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant='h2'>
+        Hola mundo
+      </Typography>
+    </>
+  )
+}
+
+export default App
---
+++ src/FirstApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstApp() {
+  return (
+    <div>
+      <h1>First App</h1>
+    </div>
+  )
+}
+
+export default FirstApp
