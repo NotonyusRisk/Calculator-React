0b0a6090c93f149223f291425d22f1e090673a07
Antonio <antoniojrudass@gmail.com>
2024-05-11 11:29:15 -0500

    estructura de carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import FirstApp from './FirstApp.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Components/App.jsx';
+import FirstApp from './Components/FirstApp.jsx';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
